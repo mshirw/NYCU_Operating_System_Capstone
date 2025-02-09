1eb047a79425860066b0d74c7f5d2a656b2c8c35
mshirw <abaocc4460@gmail.com>
2025-02-10 00:28:04 +0800

    Add memory allocation functions and initialize memory structures

--- lab2/kernel/include/memalloc.h
+++ lab2/kernel/include/memalloc.h
@@ -1,5 +1,8 @@
 #ifndef _MEMALLOC_H
 #define _MEMALLOC_H
+#include "utils.h"
+
+//buddy system memory allocation
 
 #define ARM_DRAM_START 0x10000000
 #define ARM_DRAM_END 0xBFFFFFFF
@@ -8,7 +11,22 @@
 #define UART_RECEIVE_BUFFER_SIZE 1024
 #define UART_TRANSMIT_BUFFER_BASE (UART_RECEIVE_BUFFER_BASE + UART_RECEIVE_BUFFER_SIZE)
 #define UART_TRANSMIT_BUFFER_SIZE 1024
+#define MEMINFO_BASE (UART_TRANSMIT_BUFFER_BASE + UART_TRANSMIT_BUFFER_SIZE)
+#define MEMINFO_SIZE 0x1000
+#define MBUF_BASE (MEMINFO_BASE + MEMINFO_SIZE)
+#define MBUF_SIZE 0x10000//64KB
+
+typedef struct frame_array
+{
+    uint32 frame_start;
+    uint16 frame_size;//in bytes
+    uint16 frame_num;
+    uint8 frame_tag;
+    uint8 is_free;
+
+} frame_array;
 
 void *malloc(unsigned long size);
+void memalloc_init();
 
 #endif  /*_MEMALLOC_H */
--- lab2/kernel/src/memalloc.c
+++ lab2/kernel/src/memalloc.c
@@ -15,3 +15,22 @@ void *malloc(unsigned long size)
 
     return (void *)mem;
 }
+
+void memalloc_init()
+{
+    // Initialize memory
+    unsigned long int *mem = (unsigned long int *)MBUF_BASE;
+    for(int i = 0; i < MBUF_SIZE / sizeof(unsigned long int); i++)
+    {
+        mem[i] = 0;
+    }
+
+    uint16 frame_list[16] = {1,1,1,1,2,2,4,4,8,8,16,16,32,32,64,64};
+    // Initialize frame_array
+    frame_array *frame = (frame_array *)MEMINFO_BASE;
+
+    for(int i = 0; i < 16; i++)
+    {
+        
+    }
+}
